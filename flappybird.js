19f5d96f3bd74e8d5cb105d818dcb57db724e635
OniiSama <111474678+89-oniichan@users.noreply.github.com>
2025-11-07 00:32:33 +0530

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -420,7 +420,7 @@ function moveBird(e) {
     }
 
     //jump (reduced for easier control)
-    velocityY = -4.5;
+    velocityY = -4.0;
 
     // Play jump sound
     playJumpSound();
@@ -432,3 +432,4 @@ function detectCollision(a, b) {
            a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
            a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
 }
+
